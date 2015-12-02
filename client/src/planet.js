5770bc4120e551a49d829cf788e288f0cab13eef
Jakob Svane Knudsen <jk@localhost.localdomain>
2015-12-02 19:55:44 +0100

    Prototype system work.

---
+++ client/src/planet.js
@@ -0,0 +1,42 @@
+var rocket = rocket || {};
+
+rocket.Planet = function (game, scale, parentBody, orbitRadius, orbitSpeed, spinSpeed) {
+
+    var sprite = game.add.sprite(
+        parentBody.x + orbitRadius,
+        parentBody.y,
+        'red'
+    );
+
+    sprite.scale.setTo(scale, scale);
+
+    game.physics.p2.enable(sprite);
+    var body = sprite.body;
+    body.setCircle((sprite.width - 400*scale) / 2, 0, 0, 0);
+    body.collideWorldBounds = false;
+    body.static = true;
+
+    body.setCollisionGroup(game.physics.p2.collisionGroups[0]);
+    body.collides(game.physics.p2.collisionGroups[0]);
+
+    this.orbitAngle = 0;
+    this.orbitRadius = orbitRadius;
+    this.orbitSpeed = orbitSpeed;
+    this.spinSpeed = spinSpeed;
+    this.game = game;
+    this.sprite = sprite;
+    this.body = body;
+    this.parentBody = parentBody;
+};
+
+rocket.Planet.preload = function (game) {
+    game.load.image('red', 'sprites/sun/redGiant.png');
+};
+
+rocket.Planet.prototype.update = function () {
+    this.orbitAngle += this.orbitSpeed % (Math.PI*2);
+    this.body.x = this.parentBody.x + Math.cos(this.orbitAngle) * this.orbitRadius;
+    this.body.y = this.parentBody.y - Math.sin(this.orbitAngle) * this.orbitRadius;
+
+    this.body.rotation += this.spinSpeed;
+};
---
+++ client/src/rocket.js
@@ -0,0 +1,38 @@
+var rocket = rocket || {};
+
+rocket.Rocket = function (game) {
+
+    var sprite = game.add.sprite(0, 2000, 'rocket');
+
+    game.physics.p2.enable(sprite);
+    var body = sprite.body;
+    body.collideWorldBounds = false;
+    body.setCollisionGroup(game.physics.p2.collisionGroups[0]);
+    body.collides(game.physics.p2.collisionGroups[0]);
+
+    this.game = game;
+    this.sprite = sprite;
+    this.body = body;
+}
+
+rocket.Rocket.preload = function (game) {
+    game.load.image('rocket', 'sprites/rockets/s1.png');
+};
+
+rocket.Rocket.prototype.update = function () {
+    var game = this.game,
+        body = this.body,
+        cursors = game.input.keyboard.createCursorKeys();
+
+    if (cursors.up.isDown) {
+        body.thrust(400);
+    }
+
+    if (cursors.left.isDown) {
+        body.angularForce -= 14;
+    }
+
+    if (cursors.right.isDown) {
+        body.angularForce += 14;
+    }
+};
