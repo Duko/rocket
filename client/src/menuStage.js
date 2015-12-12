4052f36f6252e482901549eaffa8ca289221d9a3
Lars Præstegaard <lars@chooselife.dk>
2015-12-13 00:43:36 +0100

    new spritesheet and json for options menu

--- client/src/menuStage.js
+++ client/src/menuStage.js
@@ -6,7 +6,8 @@ astro.MenuStage = function() {
 astro.MenuStage.prototype.preload = function(game) {
 
     astro.Starfield.preload(this);
-    game.load.image('menuplanet', 'sprites/bodies/blue.png');
+    game.load.image('planet', 'sprites/bodies/blue.png');
+    game.load.image('blackHole', 'sprites/bodies/blackhole.png');
     game.load.image('logo','sprites/misc/logo.png');
     game.load.image('btnStart','sprites/misc/btn_start.png');
 
@@ -17,6 +18,7 @@ astro.MenuStage.prototype.preload = function(game) {
 astro.MenuStage.prototype.create = function(game) {
     // console.log('menustage create');
 
+    //Menu music
     music = game.add.audio('musicTitle');
     music.volume = 0.1;
     music.play();
@@ -30,49 +32,31 @@ astro.MenuStage.prototype.create = function(game) {
     logo.scale.setTo(((100/1080)*game.height)/100,((100/1080)*game.height)/100);
     game.add.tween(logo).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true);
 
-    menuplanet = game.add.sprite(game.width/2,(game.height*1.4), 'menuplanet');
-    menuplanet.anchor.setTo(0.5, 0.5);
-    menuplanet.scale.setTo(((100/1080)*game.height)/100,((100/1080)*game.height)/100);
+    planet = game.add.sprite(game.width/2,(game.height*1.4), 'planet');
+    planet.anchor.setTo(0.5, 0.5);
+    planet.scale.setTo(((100/1080)*game.height)/100,((100/1080)*game.height)/100);
+
+    blackHole = game.add.sprite(game.width/4,(game.height*0.4), 'blackHole');
+    blackHole.anchor.setTo(0.5, 0.5);
+    blackHole.scale.setTo(((50/1080)*game.height)/100,((50/1080)*game.height)/100);
 
     btnStart = game.add.sprite(game.width/2,game.height*0.55, 'btnStart');
     btnStart.alpha = 0;
     btnStart.anchor.setTo(0.5, 0.5);
     btnStart.scale.setTo(((100/1080)*game.height)/100,((100/1080)*game.height)/100);
-    game.add.tween(btnStart).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true);
+    game.add.tween(btnStart).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 1400);
     btnStart.inputEnabled = true;
     btnStart.input.useHandCursor = true;
     btnStart.events.onInputDown.add(this.startGameClick, this);
-
-    // var posX = 0,
-    //     menu = this.add.group();
-
-    // this.menu = menu;
-
-
-
-    // var startText = astro.createText(this, 20, "START GAME");
-    //     startText.x = this.world.centerX;
-    //     startText.y = 0;
-    //     startText.inputEnabled = true;
-    //     startText.input.useHandCursor = true;
-    //     startText.anchor.setTo(.5,.5);
-    //     startText.events.onInputDown.add(this.startGameClick, this);
-    //     startText.alpha = 0;
-    //     game.add.tween(startText).to( { alpha: 1 }, 3000, Phaser.Easing.Linear.None, true);
-
-
-
-    // menu.add(startText);
-    // menu.x = 0;
-    // menu.y = game.height*0.55;
-
 
 };
 
 astro.MenuStage.prototype.update = function(game) {
     // console.log('menustage update');
 
-    menuplanet.angle += 0.01;
+    planet.angle += 0.01;
+    blackHole.angle += -0.1;
+
 
 };
 
